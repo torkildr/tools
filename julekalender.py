a725b36600ec15dcbd9431a5e2c2de05d9c58b42
Torkild Retvedt <torkild@retvedt.no>
2010-12-13 12:53:25 +0100

    Merged (and fixed...) patch by bjornars

--- julekalender.py
+++ julekalender.py
@@ -74,30 +74,47 @@ def ask_question(number):
     file.close()
 
 def show_scores():
-    scores = os.path.join(savedir, "%s.scores.txt" % time.strftime("%Y"))
+    # finn nyeste score
+    day_score = os.path.join(savedir, "%s.scores.%%d.txt" % time.strftime("%Y"))
+    luke_nr = -1
+    for dag in range(1, 24):
+        day_file = os.path.join(savedir, day_score % dag)
+        if os.path.isfile(day_file):
+            luke_nr = dag
 
-    try:
-        mtime=time.localtime(os.path.getmtime(scores))
-        lastmod = " [Luke %d]" % (int(time.strftime("%d", mtime))-1)
-    except:
-        lastmod = ""
-
-    if not os.path.isfile(scores):
+    if luke_nr == -1:
         print "Fant ikke noen resutater."
         sys.exit(0)
     
-    results = []
+    lastmod = " [Luke %d]" % luke_nr
 
-    file = open(scores, "r")
-    for line in file:
-        tmp = line.strip().split(":")
-        results.append( (tmp[0], float(tmp[1])) )
+    results = {}
+    last_results = {}
     
-    results = sorted(results, key=operator.itemgetter(1), reverse=True)
-    results = ", ".join(["%s: %g" % (x, y) for x, y in results])
+    # finn poeng for hver dag, og legg sammen
+    for dag in range(1, dag+1):
+        day_file = os.path.join(savedir, day_score % dag)
+        if os.path.isfile(day_file):
+            for line in open(day_file):
+                if len(line.strip()) == 0:
+                	continue
 
-    print r + "Resultater: " + c + b + results + b + lastmod
-    file.close()
+                nick, score = line.split(":")
+                score = float(score)
+                if nick in results: 
+                    results[nick] += score
+                else:
+                    results[nick] = score
+
+                if dag == luke_nr:
+                    last_results[nick] = score
+
+    # output!
+    results = sorted(results.iteritems(), key=operator.itemgetter(1), reverse=True)
+    results = ", ".join(["%s: %g" % (x, y) for x, y in results])
+    last_results = sorted(last_results.iteritems(), key=operator.itemgetter(1), reverse=True)
+    last_results = ", ".join(["%s: %g" % (x, y) for x, y in last_results])
+    print r + "Resultater: " + c + b + results + b + " | Ny: " + c + last_results + r + lastmod
 
 def check_num(number):
     if number > 24 or number < 1:
