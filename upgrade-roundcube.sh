242b8664b44f7558869eabc016e9b9bb877e4bec
Torkild Retvedt <torkild@retvedt.no>
2011-01-13 11:09:16 +0100

    Script for upgrading roundcube mail version

---
+++ upgrade-roundcube.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: source destination"
+    exit 0
+fi
+
+base=$(basename ${1})
+dest=$(basename ${2})
+
+echo "Upgrading installation in ${dest} with files from ${base}"
+
+for dir in "bin" "SQL" "program" "installer" "skins/default" "plugins"; do
+    echo "${base}/${dir} -> ${dest}/${dir}/"
+    cp -R ${base}/${dir}/* ${dest}/${dir}/
+done
+
+echo "${base}/ -> ${dest}/"
+find ${base} -maxdepth 1 -type f -exec cp {} ${dest}/ \;
+
+cd ${dest}
+
+echo
+echo "Running update script"
+./bin/update.sh
+
+cd - > /dev/null
+
